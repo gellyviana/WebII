de42ef36c48371858d9f375d3bc48cf17914f9cb
gellyviana <gellyviana@outlook.com>
2019-09-26 15:01:42 -0300

    Codigo para request e response da página

--- Reservas/src/br/ufrn/imd/dominio/Usuario.java
+++ Reservas/src/br/ufrn/imd/dominio/Usuario.java
@@ -1,5 +1,34 @@
 package br.ufrn.imd.dominio;
 
+/*
+ * @named Gelly Viana Mota
+ * Classe responsavel em ser o controler de Usuario
+ * 
+ */
 public class Usuario {
+	
+	private String nome;
+	private String login;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 
 }
