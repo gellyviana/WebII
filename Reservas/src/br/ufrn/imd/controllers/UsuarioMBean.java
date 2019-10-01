9ee769bd6bf913438b982b2d0525ace7b351e3ee
gellyviana <gellyviana@outlook.com>
2019-10-01 12:37:10 -0300

    Adicionado o persistence.xml e devidamente conectando com o banco PostgreSQL

--- Reservas/src/br/ufrn/imd/controllers/UsuarioMBean.java
+++ Reservas/src/br/ufrn/imd/controllers/UsuarioMBean.java
@@ -1,5 +1,61 @@
 package br.ufrn.imd.controllers;
 
-public class UsuarioMBean {
+import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.ufrn.imd.dominio.Usuario;
+import br.ufrn.imd.repositorio.UsuarioRepositorio;
+
+
+@Named("usuarioMBean")
+@SessionScoped
+public class UsuarioMBean implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private UsuarioRepositorio usuarioRepositorio;
+	
+	private Usuario usuario;
+	private Usuario usuarioLogado;
+	
+	public UsuarioMBean() {
+		usuario = new Usuario();
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Usuario getUsuarioLogado() {
+		return usuarioLogado;
+	}
+	public void setUsuarioLogado(Usuario usuarioLogado) {
+		this.usuarioLogado = usuarioLogado;
+	}
+
+	public String logar() {
+		Usuario usuarioBd = usuarioRepositorio.getUsuario(usuario.getLogin(), usuario.getSenha());
+		
+		if(usuarioBd != null) {
+			usuarioLogado = usuarioBd;
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", usuarioLogado);
+			return "/pages/index.jsf";
+		}else {
+			FacesMessage msg = new FacesMessage("Usuario não encontrado");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			FacesContext.getCurrentInstance().addMessage("", msg);
+			return null;
+		}
+	}
 }
--- Reservas/src/br/ufrn/imd/repositorio/UsuarioRepositorio.java
+++ Reservas/src/br/ufrn/imd/repositorio/UsuarioRepositorio.java
@@ -1,5 +1,30 @@
 package br.ufrn.imd.repositorio;
 
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import br.ufrn.imd.dominio.Usuario;
+
+@Stateless
 public class UsuarioRepositorio {
+	
+	@PersistenceContext
+	private EntityManager em;
+		
+	public Usuario getUsuario(String login, String senha) {
+		
+		try {
+			Query q = em.createQuery("select u from Usuario u where login = :login and senha =:senha");
+			q.setParameter("login", login);
+			q.setParameter("senha", senha);
+			return (Usuario) q.getSingleResult();
+		}catch(NoResultException e) {
+			return null;
+		}
+	}
 
 }
